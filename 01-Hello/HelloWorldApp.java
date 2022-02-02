90968edcc8fa2ee2980889837f934cea5a85359c
João Cota <31344255+JoaoCota@users.noreply.github.com>
2022-02-02 15:03:41 -0300

    Delete 01-Hello directory

--- 01-Hello/HelloWorldApp.java
+++
@@ -1,9 +0,0 @@
-/**
- * A classe HelloWorldApp implementa um aplicativo que
- * simplesmente imprime "Hello World!" à saída padrão.
- */
-class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Exibe a string.
-    }
-}
