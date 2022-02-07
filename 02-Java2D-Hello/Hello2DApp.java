9450830d5ca1512e4bd2c738a7851236f5a0651d
João Cota <31344255+JoaoCota@users.noreply.github.com>
2022-02-07 18:48:29 -0300

    Add files via upload

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -25,10 +25,19 @@ class Hello2DFrame extends JFrame {
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
         int w = getWidth();
         int h = getHeight();
+        g2d.setColor(Color.black);
+        g2d.fillRect(0, 0, w, h);
+        g2d.setPaint(Color.white);
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
+        g2d.setPaint(Color.yellow);
+        g2d.drawLine(50,150, 300,150);
+        g2d.drawLine(50,250, 300,250);
+        g2d.drawLine(50,150, 50,250);
+        g2d.drawLine(300,150, 300,250);
+        g2d.setPaint(Color.green);
+        g2d.drawString("LP2 - Joao Cota", 135, 200);
     }
 }
