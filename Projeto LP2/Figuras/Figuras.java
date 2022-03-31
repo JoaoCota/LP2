11ef9bd2ee87aa0889c1ea3d8c753d33ba8b848e
JoaoCota <joaocota1999@gmail.com>
2022-03-31 20:11:55 -0300

    Inclusão de cor, arrastar e clicar.

--- Projeto LP2/Figuras/Figuras.java
+++ Projeto LP2/Figuras/Figuras.java
@@ -1,16 +1,28 @@
 package Figuras;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
-public abstract class Figuras {
+public abstract class Figuras{
     public int x, y;
     public int w, h;
+    public Color corContorno;
 
-    public Figuras(int x, int y, int w, int h) {
+    public Figuras(int x, int y, int w, int h, Color corContorno){
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.corContorno = corContorno;
+    }
+
+    public void drag (int dx, int dy){
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public boolean clicked (int x, int y){
+        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
 
     public abstract void paint (Graphics g);
